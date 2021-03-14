fd53af161808e269f0d898ca28d9f80e24cf72f9
Kreedzt <zhaozisong1@live.com>
2021-03-14 20:19:04 +0800

    :tada: Kreedzt - initial commit

--- src/main/kotlin/PluginMain.kt
+++ src/main/kotlin/PluginMain.kt
@@ -1,26 +1,90 @@
 package org.example.mirai.plugin
 
+import kotlinx.coroutines.SupervisorJob
+import net.mamoe.mirai.console.command.CommandManager.INSTANCE.register
+import net.mamoe.mirai.console.command.CommandManager.INSTANCE.unregister
+import net.mamoe.mirai.console.command.CommandSender
+import net.mamoe.mirai.console.command.CommandSender.Companion.toCommandSender
+import net.mamoe.mirai.console.command.CompositeCommand
+import net.mamoe.mirai.console.command.SimpleCommand
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import net.mamoe.mirai.contact.Member
+import net.mamoe.mirai.event.EventHandler
+import net.mamoe.mirai.event.ListenerHost
+import net.mamoe.mirai.event.events.FriendMessageEvent
+import net.mamoe.mirai.message.data.Image
 import net.mamoe.mirai.utils.info
+import net.mamoe.mirai.event.events.MessageEvent
+import net.mamoe.mirai.event.globalEventChannel
 
 object PluginMain : KotlinPlugin(
     JvmPluginDescription(
-        id = "org.example.mirai-plugin",
-        name = "ExamplePlugin",
-        version = "0.1.0"
+        id = "org.example.kreedzt-plugin",
+        name = "TestPlugin-v0",
+        version = "0.1.1"
     ) {
-        author("作者名称或联系方式")
+        author("Kreedzt")
 
         info("""
-            这是一个测试插件, 
-            在这里描述插件的功能和用法等.
+            Kreedzt:test
         """.trimIndent())
-
-        // author 和 info 可以删除.
     }
 ) {
     override fun onEnable() {
         logger.info { "Plugin loaded" }
+
+        MySimpleCommand.register()
+        MyCompositeCommand.register()
+
+        this.globalEventChannel().subscribeAlways<MessageEvent>{event ->
+            logger.info("eee")
+            logger.info(event.toString())
+            logger.info(event.message.toString())
+            this.toCommandSender().sendMessage("Test Res")
+        }
+
+        this.globalEventChannel().subscribeAlways<FriendMessageEvent>{event ->
+            logger.info("This is friendMessage: ${event.senderName}")
+            event.toCommandSender().sendMessage("朋友消息")
+        }
+    }
+
+    override fun onDisable() {
+        MySimpleCommand.unregister()
+        MyCompositeCommand.unregister()
+    }
+}
+
+// 简单指令
+object MySimpleCommand: SimpleCommand(
+    PluginMain,
+    "foo",
+    "私聊",
+    description = "示例指令",
+) {
+    @Handler
+    suspend fun CommandSender.handle(int: Int, str: String) {
+        sendMessage("/foo 的第一个参数是 $int, 第二个是 $str")
+    }
+}
+
+object MyCompositeCommand: CompositeCommand(
+    PluginMain, "manage",
+    description = "示例复杂指令"
+) {
+    @SubCommand
+    suspend fun CommandSender.mute(target: Member, duration: Int) {
+        sendMessage("/manage mute 被调用了, 参数为; $target, $duration")
+    }
+
+    @SubCommand
+    suspend fun CommandSender.list() {
+        sendMessage("/manage list 被调用了")
+    }
+
+    @SubCommand
+    suspend fun CommandSender.test(image: Image) {
+        sendMessage("/manage image 被调用了, 图片是 ${image.imageId}")
     }
 }
